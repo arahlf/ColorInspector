71cc0552f57e5881b84f357f0270c0334980507a
Alan Rahlf <arahlf@gmail.com>
2011-08-14 23:32:15 -0500

    Finally got rid of the Hooking class.

--- ColorInspector/MouseMoveHook.cs
+++ ColorInspector/MouseMoveHook.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
 
 namespace ColorInspector
 {
@@ -6,7 +8,12 @@ namespace ColorInspector
     {
         public MouseMoveHook(IMouseMoveListener mouseMovelistener) {
             listener = mouseMovelistener;
-            hook = Hooking.CreateLowLevelMouseHook(listener);
+            hookProc = HookCallback; // if an instance-level delegate is not used, windows will silently remove the hook...
+
+            using (Process currentProcess = Process.GetCurrentProcess())
+            using (ProcessModule currentModule = currentProcess.MainModule) {
+                hook = User32.SetWindowsHookEx(WH_MOUSE_LL, hookProc, Kernel32.GetModuleHandle(currentModule.ModuleName), 0);
+            }
         }
 
         public void Dispose() {
@@ -14,6 +21,21 @@ namespace ColorInspector
             hook = IntPtr.Zero;
         }
 
+        public IntPtr HookCallback(int nCode, IntPtr wParam, IntPtr lParam) {
+            if (nCode >= 0 && MouseMessages.WM_MOUSEMOVE == (MouseMessages)wParam) {
+                MSLLHookStruct hookStruct = (MSLLHookStruct) Marshal.PtrToStructure(lParam, typeof(MSLLHookStruct));
+
+                Utils.ConstrainToDesktop(ref hookStruct.pt);
+
+                listener.OnMouseMove(hookStruct.pt.x, hookStruct.pt.y);
+            }
+
+            return User32.CallNextHookEx(hook, nCode, wParam, lParam);
+        }
+
+        private const int WH_MOUSE_LL = 14; // specifies a low-level mouse hook
+
+        private User32.HookProc hookProc;
         private IntPtr hook;
         private IMouseMoveListener listener;
     }
